aee52b6e88cc22e96ff0a0dcfee11990cb3f79a6
Jack Manuel <145254559+jackmanuelcoding@users.noreply.github.com>
2023-10-12 10:25:46 +1000

    Order leads by company after country

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -2,7 +2,7 @@ class ConsolesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @leads = Lead.all.order(:country)
+    @leads = Lead.all.order(:country, :company)
 
     authorize :console, :show?
   end
