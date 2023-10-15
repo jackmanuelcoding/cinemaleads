56a4f735d6b0738b6fe0f37df4a6d25cd6e8217f
Jack Manuel <145254559+jackmanuelcoding@users.noreply.github.com>
2023-10-15 11:54:50 +1000

    Configure production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,10 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = {
+    server_api_token:
+      Rails.application.credentials.dig(:postmark, server_api_token)
+  }
 end
