58bbec6c95293e504a4153bbd09b549d0d5f6ed0
Jack Manuel <145254559+jackmanuelcoding@users.noreply.github.com>
2023-10-16 13:57:28 +1000

    Redirect to canonical url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  r301 /.*/,
+       "cinemaleads.com$&",
+       if: Proc.new { |rack_env| rack_env["SERVER_NAME"] != "cinemaleads.com" }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
