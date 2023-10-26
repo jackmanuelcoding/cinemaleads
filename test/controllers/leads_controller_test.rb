249db3ae39341c411d7e4937724d02622a7fd1ea
Jack Manuel <145254559+jackmanuelcoding@users.noreply.github.com>
2023-10-26 10:23:51 +1000

    Beef up tests

--- test/controllers/leads_controller_test.rb
+++ test/controllers/leads_controller_test.rb
@@ -6,6 +6,7 @@ class LeadsControllerTest < ActionDispatch::IntegrationTest
     @unsubscribed = users(:unsubscribed)
     @customer = users(:customer)
     @lead = leads(:one)
+    @lead_three = leads(:three)
 
     @admin.confirm
     @unsubscribed.confirm
@@ -15,7 +16,7 @@ class LeadsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     sign_in @admin
 
-    get leads_url
+    get leads_url(@lead, @lead_three)
     assert_response :success
   end
 
@@ -26,11 +27,59 @@ class LeadsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create lead" do
+    sign_in @admin
+
+    assert_difference("Lead.count", 1) do
+      post leads_url,
+           params: {
+             lead: {
+               country: "MyString",
+               company: "MyString",
+               website: "MyString",
+               linkedin: "MyString",
+               category: "distributors"
+             }
+           }
+    end
+    assert_redirected_to console_url
+    assert_equal "MyString has been added to leads.", flash[:notice]
+  end
+
   test "should get edit" do
     sign_in @admin
 
     get edit_lead_url(@lead)
     assert_response :success
+  end
+
+  test "should update lead" do
+    sign_in @admin
+
+    assert_difference("Lead.count", 0) do
+      put lead_url(@lead),
+          params: {
+            lead: {
+              country: "United States",
+              company: "MyString",
+              website: "MyString",
+              linkedin: "MyString",
+              category: "distributors"
+            }
+          }
+    end
+    assert_redirected_to console_url
+    assert_equal "MyString has been updated.", flash[:notice]
+  end
+
+  test "should destroy lead" do
+    sign_in @admin
+
+    assert_difference("Lead.count", -1) do
+      delete lead_url(@lead)
+      assert_redirected_to console_url
+      assert_equal "MyString has been removed from leads.", flash[:notice]
+    end
   end
 
   test "should redirect from leads to checkout when user unsubscribed" do
--- test/models/lead_test.rb
+++ test/models/lead_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class LeadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @lead = leads(:one)
+    @lead_two = leads(:two)
+    @lead_four = leads(:four)
+  end
+
+  test "should be valid when all fields are present" do
+    assert @lead.valid?
+  end
+
+  test "should be invalid when no fields are present" do
+    assert @lead_two.invalid?
+  end
+
+  test "should be invalid when fields exceed 255 characters" do
+    assert @lead_four.invalid?
+  end
 end
